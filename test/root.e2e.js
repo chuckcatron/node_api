5797177a1af884a3bbae0fcda7720b307d73409c
ChuckDev <charles.catron@gmail.com>
2020-11-23 10:01:20 -0500

    init heroku

---
+++ test/root.e2e.js
@@ -0,0 +1,102 @@
+import request from "supertest";
+import express from "express";
+import { rootRouter } from "../routes";
+
+const server = express();
+
+server.use("/v1", rootRouter);
+
+describe("GET /v1", function () {
+  it("responds with json", function (done) {
+    request(server)
+      .get("/v1")
+      .expect("Content-Type", /json/)
+      .expect((res) => {
+        res.body.headers = {};
+      })
+      .expect(
+        200,
+        {
+          baseUrl: "/v1",
+          headers: {},
+          host: "127.0.0.1",
+          ip: "::ffff:127.0.0.1",
+          message: "Handling GET request",
+          method: "GET",
+          path: "/",
+        },
+        done
+      );
+  });
+});
+
+describe("GET /v1", function () {
+  it("responds with json", function (done) {
+    request(server)
+      .get("/v1")
+      .expect("Content-Type", /json/)
+      .expect((res) => {
+        res.body.headers = {};
+      })
+      .expect(
+        200,
+        {
+          baseUrl: "/v1",
+          headers: {},
+          host: "127.0.0.1",
+          ip: "::ffff:127.0.0.1",
+          message: "Handling GET request",
+          method: "GET",
+          path: "/",
+        },
+        done
+      );
+  });
+});
+
+describe("GET /v1", function () {
+  it("responds with json", function (done) {
+    request(server)
+      .get("/v1")
+      .expect("Content-Type", /json/)
+      .expect((res) => {
+        res.body.headers = {};
+      })
+      .expect(
+        200,
+        {
+          baseUrl: "/v1",
+          headers: {},
+          host: "127.0.0.1",
+          ip: "::ffff:127.0.0.1",
+          message: "Handling GET request",
+          method: "GET",
+          path: "/",
+        },
+        done
+      );
+  });
+});
+describe("GET /v1", function () {
+  it("responds with json", function (done) {
+    request(server)
+      .get("/v1")
+      .expect("Content-Type", /json/)
+      .expect((res) => {
+        res.body.headers = {};
+      })
+      .expect(
+        200,
+        {
+          baseUrl: "/v1",
+          headers: {},
+          host: "127.0.0.1",
+          ip: "::ffff:127.0.0.1",
+          message: "Handling GET request",
+          method: "GET",
+          path: "/",
+        },
+        done
+      );
+  });
+});
